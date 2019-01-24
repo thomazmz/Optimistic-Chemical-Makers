614e21093b3d5d4db434a2621089ecfe3b9abae9
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-01-24 18:14:53 -0200

    Add controller, model and repository classes for User

---
+++ src/main/java/com/OptimisticChemicalMakers/MapFood/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.OptimisticChemicalMakers.MapFood.controllers;
+
+import com.OptimisticChemicalMakers.MapFood.models.GeographicPosition;
+import com.OptimisticChemicalMakers.MapFood.models.User;
+import com.OptimisticChemicalMakers.MapFood.repositories.UserRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    // Properties
+
+    private UserRepository userRepository;
+
+    // Constructors
+
+    public UserController(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+
+    }
+
+    // Methods (Actions)
+
+    // GET /users
+    // Return all users persisted on the database
+    @RequestMapping(value = "/users", method = RequestMethod.GET, produces = "application/json")
+    public Iterable<User> getUsers() {
+
+        return userRepository.findAll();
+
+    }
+
+}
---
+++ src/main/java/com/OptimisticChemicalMakers/MapFood/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.OptimisticChemicalMakers.MapFood.repositories;
+
+import com.OptimisticChemicalMakers.MapFood.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+}
+
+
