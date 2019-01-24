f94f59a5356be0e0f77c7b20aa5ba7ec1e9a8144
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-01-24 18:19:30 -0200

    Add model and repository classes for Store

---
+++ src/main/java/com/OptimisticChemicalMakers/MapFood/repositories/StoreRepository.java
@@ -0,0 +1,11 @@
+package com.OptimisticChemicalMakers.MapFood.repositories;
+
+import com.OptimisticChemicalMakers.MapFood.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StoreRepository extends CrudRepository<User, Long> {
+
+
+}
+
+
