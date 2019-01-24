dbf8ec41744f758855be1fe2c35cc3a4a5135db6
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-01-24 18:18:28 -0200

    Add model and repository classes for Product

---
+++ src/main/java/com/OptimisticChemicalMakers/MapFood/models/Product.java
@@ -0,0 +1,59 @@
+package com.OptimisticChemicalMakers.MapFood.models;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Product {
+
+    // Properties
+
+    @Id                                                 // It tells the JPA that it is an ID
+    @GeneratedValue(strategy = GenerationType.AUTO)     // It tells the JPA how to autogenerate the ID value
+    private Long id;
+
+    private String description;
+
+    private BigDecimal price;
+
+    @ManyToOne
+    private Store store;
+
+    // Constructors
+
+    public Product(Store store, String description, BigDecimal price) {
+        this.store = store;
+        this.description = description;
+        this.price = price;
+    }
+
+    // Get Methods
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    // Set Methods
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+}
+
