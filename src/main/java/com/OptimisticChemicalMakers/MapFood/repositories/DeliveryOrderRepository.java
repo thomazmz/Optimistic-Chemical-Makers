a7189a310b62f6bdb6fffb3d80d3a2b645f281cc
Lauro Correa <laurocj@gmail.com>
2019-02-08 19:13:51 -0200

    Revert "in progress"
    This reverts commit d61e8b371ced5dceb8f7874cab8797df7045bd45.

--- src/main/java/com/OptimisticChemicalMakers/MapFood/repositories/DeliveryOrderRepository.java
+++ src/main/java/com/OptimisticChemicalMakers/MapFood/repositories/DeliveryOrderRepository.java
@@ -1,12 +1,7 @@
 package com.OptimisticChemicalMakers.MapFood.repositories;
 
 import com.OptimisticChemicalMakers.MapFood.models.DeliveryOrder;
-
-import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
 
 public interface DeliveryOrderRepository extends CrudRepository<DeliveryOrder, Long> {
-
-	List<DeliveryOrder> findByPreparedAtIsNull();
 }
