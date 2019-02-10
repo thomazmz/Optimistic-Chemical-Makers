7966a34afdf93b53f7f022d9861c9b0f1317c36f
thomazmz <thomaz.zandonotto@gmail.com>
2019-02-10 14:21:52 -0200

    Reorganize getProductsByStore route

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,18 +1,27 @@
-INSERT INTO `store`(`protocol`, `name`, `city`, `longitude`, `latitude`, `dish_description`) VALUES ('5640f4538237d9c4aaf3b751c4d11769b3fc1e3165ed3b912c508768f7fc15fd', 'Monteiro´s Lanches', 'SAO PAULO', -46.64097300, -23.60365800, 'Lanches');
-INSERT INTO `store`(`protocol`, `name`, `city`, `longitude`, `latitude`, `dish_description`) VALUES ('a660f6374ab1a743468f576cb89e3a8cd9b9776a363ada7002a5db6066a8b572', 'True Food', 'SAO PAULO', -46.65790800, -23.55442300, 'Comida Saudável');
-INSERT INTO `store`(`protocol`, `name`, `city`, `longitude`, `latitude`, `dish_description`) VALUES ('07fc65555705243de378cc34222909fe9f44ad055b855ec8b7b9b8df86d70874', 'Testador', 'SAO PAULO', -46.68878200, -23.60052900, 'Fit');
-INSERT INTO `store`(`protocol`, `name`, `city`, `longitude`, `latitude`, `dish_description`) VALUES ('adc9c524d56679d7fb7ffd9d5b2a3eaba5209758751576f2ffa5d2a6432565f8', 'Restaurante dus Guri', 'SAO PAULO', -46.76341500, -23.58710100, 'Dia do Lixo');
-INSERT INTO `store`(`protocol`, `name`, `city`, `longitude`, `latitude`, `dish_description`) VALUES ('6b511620f33db24724ba2b2fe6c31d78a4247c6360f83c785afe181ac15c6996', 'Restaurante du cupinxa', 'SAO PAULO', -46.74373400, -23.64276300, 'Vegan');
+INSERT INTO `store`(`id`, `protocol`, `name`, `city`, `latitude`, `longitude`, `dish_description`)
+VALUES (1, '416296f0dc3c4094a0a27a5584df855a', 'Mc Donalds Silva Só', 'PORTO ALEGRE', -30.042617, -51.204800, 'Lanches');
+
+INSERT INTO `store`(`id`, `protocol`, `name`, `city`, `latitude`, `longitude`, `dish_description`)
+VALUES (2, '9df51ec38e9d4b079c24332cd0dc44b5', 'Coqueiro Lanches', 'PORTO ALEGRE', -30.115465, -51.254236, 'Xis');
+
+INSERT INTO `store`(`id`, `protocol`, `name`, `city`, `latitude`, `longitude`, `dish_description`)
+VALUES (3, 'f7fdc4f02159436e9a63d0a76c288040', 'Subway Agronomia', 'PORTO ALEGRE', -30.061968, -51.162254, 'Sanduíches');
 
 
-INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Galinhada executivo', '00002d8401f2d47cef5bb54b4e1cea226ec5155f2b5fb5fab4163b71d3eeb281', 1, 'Almoço por até R$ 13,90', 13.90);
-INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Hamgurguer', '00006fa61eebf64da44f96788d6efb4a03f3dbcc6b0cfce08fce3e5095f364f9', 2, 'teste ruim', 25.90);
-INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Churrasco', '00007964e46724000380162eb5bb541bd9213d309e766634396c669c05236fe9', 3, 'teste idiota', 1.90);
-INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Salada Zika', '0000ba772533c4242eb038da8b2a891d550a1199a2287085a6711728cefffcd6', 4, 'teste cagado', 3.90);
-INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Suco', '0000bfbb030501276c8b2b845c5d5bd9d40551af6e39ea2a2e52954d83aa8667', 5, 'teste zika', 43.90);
+
+INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Mc Lanche Feliz', 'e25305740fa047c78c7695bdc9a9eb86', 1, 'Para crianças', 18.90);
+INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Big Mac', '9f58b56bf809444eb1e2f7eb235233d0', 1, 'Um clássico', 25.90);
+
+INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Xis Salada', '322f5041f4b8445c8421af7e7b0e8a7e', 2, 'Xis salada com ovo', 20.90);
+INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Xis Coração', '68a6923cfc0247118be4ffc46bd1f6ac', 2, 'Xis coração com ovo', 22.90);
+
+INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('BMT', 'ed0b8df5aa134bc9aa7a2612b799d948', 3, 'Salame e presunto', 20.90);
+INSERT INTO `product`(`description`, `protocol`, `store_id`, `classification`, `price`) VALUES ('Frango Teriyaki', 'bf09c8c9f34a4e79a1d3eedbe1737035', 3, 'Frango com molho Teriyaki', 22.90);
+
 
 INSERT INTO `delivery_boy`(`longitude`, `latitude`, `protocol`) VALUES (-46.76341500, -23.58710100, '204f9ecd305044eab8f38187bbfdc3bb');
 INSERT INTO `delivery_boy`(`longitude`, `latitude`, `protocol`) VALUES (-46.68878200, -23.60052900, 'd193cf7d8419411bb83d74e64a732b23');
 INSERT INTO `delivery_boy`(`longitude`, `latitude`, `protocol`) VALUES (-46.76341500, -23.58710101, 'ebccdabe06424508ba44c5250fd64c15');
 INSERT INTO `delivery_boy`(`longitude`, `latitude`, `protocol`) VALUES (-46.64097300, -23.60365800, 'eb7e580e9602465893c98c6a5750dded');
 INSERT INTO `delivery_boy`(`longitude`, `latitude`, `protocol`) VALUES (-46.74373400, -23.64276300, 'f2c672494b5448c78a4ca75d7b29479f');
+
