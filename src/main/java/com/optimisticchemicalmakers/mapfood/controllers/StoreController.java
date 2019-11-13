ec8591610c5c6d113d2a701b515aa97696e7a69c
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2019-11-13 20:43:56 -0300

    Implement first post enpoint

--- src/main/java/com/optimisticchemicalmakers/mapfood/controllers/StoreController.java
+++ src/main/java/com/optimisticchemicalmakers/mapfood/controllers/StoreController.java
@@ -1,5 +1,8 @@
 package com.optimisticchemicalmakers.mapfood.controllers;
 
+import com.optimisticchemicalmakers.mapfood.models.Store;
+import com.optimisticchemicalmakers.mapfood.repositories.StoreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/stores")
 public class StoreController {
 
+    @Autowired
+    StoreRepository storeRepository;
+
     @GetMapping
-    public ResponseEntity<?> createStore() {
+    public ResponseEntity<?> firstEndpoint() {
         return new ResponseEntity<>("First application endpoint!", HttpStatus.ACCEPTED);
     }
 
+    @PostMapping
+    public ResponseEntity<?> createStore(final Store store) {
+        Store savedStore = storeRepository.save(store);
+        return new ResponseEntity<>(savedStore, HttpStatus.ACCEPTED);
+    }
 }
